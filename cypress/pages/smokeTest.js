8d95491ea2628d3365dd81da36c467a6630404d9
Pablo Arzu <jptomas38@gmail.com>
2023-08-04 15:15:11 -0600

    smokeTest.js: Agregando comentarios descriptivos

--- cypress/pages/smokeTest.js
+++ cypress/pages/smokeTest.js
@@ -4,18 +4,23 @@ class SmokeTest {
 
     elements = {
 
-        username: () => cy.get('#user-name'),
-        Password: () => cy.get('#password'),
-        loginBtn: () => cy.get('login-button')
+        username: () => cy.get('#user-name'), //Seleccionado el input de username
+        Password: () => cy.get('#password'), // Seleccionando el input de password
+        loginBtn: () => cy.get('login-button') //Seleccionando el boton de login
 
     }
 
-    typeUserName( username ) {
+    //Funcion para ingresar username
+    typeUserName( username ) { 
         this.elements.username().type( username )
     }
+
+    //Funcion para ingresar password
     typePassword( password ) {
         this.elements.Password().type( password )
     }
+
+    //Funcion para dar clic en el boton de login
     clickLoginBtn() {
         this.elements.loginBtn().click()
     }
