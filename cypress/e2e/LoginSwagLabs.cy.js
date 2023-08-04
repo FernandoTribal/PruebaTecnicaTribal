11782e7ca4dd82c3508eb5d365fe60d5c024ab7a
Pablo Arzu <jptomas38@gmail.com>
2023-08-04 15:38:24 -0600

    Terminando LoginTest

--- cypress/e2e/LoginSwagLabs.cy.js
+++ cypress/e2e/LoginSwagLabs.cy.js
@@ -20,6 +20,15 @@ describe('Login en Swag Labs ', () => {
         LoginSwagLabs.typePassword( password ); //ingresando password
         LoginSwagLabs.clickLoginBtn(); //haciendo clic en "Login"
 
+        LoginSwagLabs.selectElement(); //Seleccionando elemento al carrito
+        LoginSwagLabs.clickCarritoCompras(); //Seleccionando el carrito
+        cy.wait(500);
+        LoginSwagLabs.clickRemoveElement(); // Removiendo elemento del carrito
+
+        LoginSwagLabs.clickBurgerMenu(); // Haciendo click en el menu
+        LoginSwagLabs.clickLogoutBtn(); // Haciendo Logout de la app
+
+
 
 
     });
